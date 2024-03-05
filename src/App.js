7ef375cbaa5bf9083c19cdb8402574fe5fe3ddf1
Kanupriya <kanupriya0092@gmail.com>
2024-03-05 18:37:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import Graph from "./graph";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+      super();
+  }
+  componentDidMount() {
+      // console.log(window.innerHeight,"  ",window.innerWidth);
+  }
+
+  render() {
+      return (
+          <Router basename='/'>
+              <Switch>
+ 
+                  <Route path='/' component={Graph}/>
+                  
+
+              </Switch>
+          </Router>
+      );
+  }
+}
+
+export default App;
+
+//http://localhost:3000/tree_visualizer#/
+//http://localhost:3000/kanupriya1010/tree_visualizer#/
